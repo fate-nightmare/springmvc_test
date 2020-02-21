0611f99fcc9d01211feb1bb01165dff4d424c79a
fate-nightmare <2114005156@qq.com>
2020-02-21 15:37:19 +0800

    本地修改并且同事更新远程仓库

--- src/main/java/com/accp/t1/action/LoginAction.java
+++ src/main/java/com/accp/t1/action/LoginAction.java
@@ -20,6 +20,7 @@ public class LoginAction {
 		System.out.println("第一次修改");
 		System.out.println("第二次修改");
 		System.out.println("第三次修改");
+		System.out.println("第四次修改");
 		if ("admin".equals(userName) && userPwd == 123) {
 			session.setAttribute("USER", new Users(userName, userPwd));
 			message.put("code", "200");
